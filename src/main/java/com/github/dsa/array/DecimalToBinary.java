e0da5ec78f99814e46edfe8511f43fbc88c3e9be
Rohit Bavkar <rohitbavkar95@gmail.com>
2022-07-05 23:09:30 +0530

    DecimalToBinary

--- src/main/java/com/github/dsa/array/DecimalToBinary.java
+++
@@ -1,7 +0,0 @@
-package com.github.dsa.array;
-
-public class DecimalToBinary {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/main/java/dsa/array/DecimalToBinary.java
@@ -0,0 +1,25 @@
+package dsa.array;
+
+import java.util.Scanner;
+
+public class DecimalToBinary {
+    public static void main(String[] args) {
+        //Take user input
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Decimal Number");
+        int decimalNo = sc.nextInt();
+        String binaryNo = convertDecimalToBinary(decimalNo);
+        System.out.println("Binary Number is : "+binaryNo);
+    }
+    private static String convertDecimalToBinary(int decimalNo) {
+        StringBuilder binaryNo = new StringBuilder();
+        int counter = 0;
+        while(decimalNo != 0){
+            int digit = decimalNo & 1;
+            decimalNo = decimalNo >>> 1;
+            binaryNo.insert(0, digit);
+            counter++;
+        }
+        return binaryNo.toString();
+    }
+}
