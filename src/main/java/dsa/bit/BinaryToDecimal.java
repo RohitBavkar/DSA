b0da10ccdd890e7c885c93c504d77d7162139206
Rohit Bavkar <rohitbavkar95@gmail.com>
2022-07-05 23:28:42 +0530

    DecimalToBinary

--- src/main/java/dsa/bit/BinaryToDecimal.java
+++ src/main/java/dsa/bit/BinaryToDecimal.java
@@ -9,16 +9,16 @@ public class BinaryToDecimal {
         System.out.println("Enter Binary Number");
         int binaryNo = sc.nextInt();
         int decimalNo = convertBinaryToDecimal(binaryNo);
-        System.out.println("Decimal Number is : " + decimalNo);
+        System.out.println("Decimal Number is : "+decimalNo);
     }
 
     private static int convertBinaryToDecimal(int binaryNo) {
         int counter = 0;
         int decimalNo = 0;
-        while (binaryNo != 0) {
+        while(binaryNo != 0){
             int digit = binaryNo % 10;
-            decimalNo = decimalNo + (int) (digit * Math.pow(2, counter));
-            binaryNo = binaryNo / 10;
+            decimalNo = decimalNo + (int)(digit * Math.pow(2, counter));
+            binaryNo = binaryNo /10;
             counter++;
         }
         return decimalNo;
--- src/main/java/dsa/bit/DecimalToBinary.java
+++ src/main/java/dsa/bit/DecimalToBinary.java
@@ -9,13 +9,12 @@ public class DecimalToBinary {
         System.out.println("Enter Decimal Number");
         int decimalNo = sc.nextInt();
         String binaryNo = convertDecimalToBinary(decimalNo);
-        System.out.println("Binary Number is : " + binaryNo);
+        System.out.println("Binary Number is : "+binaryNo);
     }
-
     private static String convertDecimalToBinary(int decimalNo) {
         StringBuilder binaryNo = new StringBuilder();
         int counter = 0;
-        while (decimalNo != 0) {
+        while(decimalNo != 0){
             int digit = decimalNo & 1;
             binaryNo.insert(0, digit);
             decimalNo = decimalNo >>> 1;
